05525c05ba49dc8d9f4e58475277de76a73dc82d
ivanasen <ivanasenchakarov@gmail.com>
2019-07-19 15:41:23 +0300

    Remove TokenExchange

--- src/Achievements/Achievement.cs
+++ src/Achievements/Achievement.cs
@@ -1,11 +1,12 @@
+using System.Numerics;
+using ContractsCore;
+using ContractsCore.Actions;
+using ContractsCore.Contracts;
+using TokenSystem.TokenManagerBase.Actions;
+
 namespace Achievements
 {
-	using ContractsCore;
-	using ContractsCore.Actions;
-	using ContractsCore.Contracts;
-	using TokenSystem.TokenManagerBase.Actions;
-
-	public class Achievement : TokenExchange
+	public class Achievement : Contract
 	{
 		public Achievement(
 			Address address,
@@ -14,9 +15,24 @@ namespace Achievements
 			int exchangeRate,
 			Address achiever,
 			AchievementMetaData metaData)
-			: base(address, burnTokenManager, mintTokenManager, exchangeRate)
+			: base(address)
 		{
+			this.BurnTokenManager = burnTokenManager;
+			this.MintTokenManager = mintTokenManager;
+			this.ExchangeRate = exchangeRate;
+			this.Achiever = achiever;
+			this.MetaData = metaData;
 		}
+
+		public Address BurnTokenManager { get; }
+
+		public Address MintTokenManager { get; }
+
+		public int ExchangeRate { get; }
+
+		public Address Achiever { get; }
+
+		private AchievementMetaData MetaData { get; }
 
 		protected override object GetState()
 		{
@@ -28,10 +44,47 @@ namespace Achievements
 			switch (action)
 			{
 				case TokensReceivedAction receivedAction:
-
+					return this.ExchangeTokens(receivedAction);
 				default:
 					return false;
 			}
+		}
+
+		private bool ExchangeTokens(TokensReceivedAction action)
+		{
+			if (!this.CanExchange())
+			{
+				return false;
+			}
+
+			BigInteger receivedAmount = action.Tokens.TotalBalance;
+			this.OnSend(new BurnAction(
+				string.Empty,
+				this.BurnTokenManager,
+				receivedAmount,
+				this.Address));
+
+			BigInteger mintAmount = receivedAmount * this.ExchangeRate;
+			this.OnSend(new MintAction(
+				string.Empty,
+				this.MintTokenManager,
+				mintAmount,
+				this.Address));
+			this.OnSend(new TransferAction(
+				string.Empty,
+				this.MintTokenManager,
+				mintAmount,
+				this.Address,
+				this.Achiever));
+
+			return true;
+		}
+
+		private bool CanExchange()
+		{
+			//TODO: Implement time limit for rewarding achievements
+			//(May need additional feature from StrongForce)
+			throw new System.NotImplementedException();
 		}
 	}
 }
